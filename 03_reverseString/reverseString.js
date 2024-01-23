58e61c16da4245fcd9443f7f915d5aa3f9197c6c
dfang <davidfang987@gmail.com>
2024-01-22 22:13:11 -0500

    exercise 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let length = str.length;
+    let arr = str.split("");
+    let s = '';
+    for (let i = 0; i < length; ++i) {
+        s += arr[length - i - 1];
+    }
+    return s;
 };
 
 // Do not edit below this line
