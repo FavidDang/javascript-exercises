3a9f2c9b62d9f162f8de860649d6ff83273e0cba
dfang <davidfang987@gmail.com>
2024-01-23 23:55:49 -0500

    exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...nums) {
+    let len = nums.length;
+    for (let i = 0; i < len; ++i) {
+        arr = arr.filter((element) => element !== nums[i]);
+    }
+    return arr;
 };
 
 // Do not edit below this line
