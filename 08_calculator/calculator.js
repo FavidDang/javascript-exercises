d3c1fad7119d524ced08357b29448c784ed431ee
dfang <davidfang987@gmail.com>
2024-01-24 01:20:00 -0500

    exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  let sum = 0;
+	for (let i = 0; i < nums.length; ++i) {
+    sum += nums[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for (let i = 0; i < arr.length; ++i) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  let product = 1;
+  for (let i = 0; i < power; ++i) {
+    product *= base;
+  }
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let product = 1;
+  for (let i = num; i > 0; --i) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
