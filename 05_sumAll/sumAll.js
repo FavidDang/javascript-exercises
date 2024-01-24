82f4491cec2eda8f007af042bb1833c17be53abf
dfang <davidfang987@gmail.com>
2024-01-24 00:32:34 -0500

    exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(n1, n2) {
+    let sum = 0;
+    if (n1 < 0 || n2 < 0 || typeof n1 !== 'number' || typeof n2 !== 'number') return 'ERROR';
+    if (n1 > n2) {
+        n1 += n2;
+        n2 = n1 - n2;
+        n1 -= n2;
+    }
+    do {
+        sum += n1;
+        ++n1;
+    } while (n1 <= n2);
+    return sum;
 };
 
 // Do not edit below this line
