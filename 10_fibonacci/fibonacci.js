526c87acc34377aa42b530759e6983ca8bf07bd4
dfang <davidfang987@gmail.com>
2024-01-26 00:22:38 -0500

    exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n !== 'number') {
+        n = parseInt(n);
+    }
 
+    if (n === 0) {
+        return 0;
+    }
+
+    if (n < 0) {
+        return "OOPS";
+    }
+
+    let n1 = 1;
+    let n2 = 1;
+    if (n === 1 || n === 2) {
+        return n1;
+    }
+
+    let i = 2;
+    while (n !== i) {
+        n2 += n1;
+        n1 = n2 - n1;
+        ++i;
+    }
+    return n2;
 };
 
 // Do not edit below this line
