800edd41ecf166d1f9ab70531ecceafc7831358b
dfang <davidfang987@gmail.com>
2024-01-26 01:15:57 -0500

    exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxsofar = 0;
+    let max_idx = 0;
+    for (let i = 0; i < people.length; ++i) {
+        let person = people[i];
+        let year = new Date().getFullYear();
+        let age = (Object.hasOwn(person, 'yearOfDeath') ? person.yearOfDeath - person.yearOfBirth : year - person.yearOfBirth);
+        if (age > maxsofar) {
+            maxsofar = age;
+            max_idx = i;
+        }
+        console.log(age);
+    }
+    return people[max_idx];
 };
 
 // Do not edit below this line
