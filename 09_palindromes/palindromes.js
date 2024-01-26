6ac36d4b0c8312531569d5912a4be884188eb38b
dfang <davidfang987@gmail.com>
2024-01-26 00:01:33 -0500

    exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[^a-z0-9]/gi, '');
 
+    for (let i = 0; i < str.length; ++i) {
+        if (str[i] !== str[str.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
